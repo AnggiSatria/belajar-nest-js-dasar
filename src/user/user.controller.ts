4a1e84e5541bcb13c82e4e5672437aef7ae72199
AnggiSatria <anggisatria122@gmail.com>
2025-11-03 00:28:17 +0700

    feat(controller-v2): adding new information about route path default in controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
 
+// pada controller kita bisa atur default routingnya tanpa harus setup routing seperti aplikasi Express JS
+// pada case berikut user memiliki default path atau default route localhost:<port>/user
+// namun kita juga dapat mengosongkan atau menambahkan agar default seperti yang kita inginkan
+// kita tinggal ubah Detractor @Controller('user') menjadi @Controller('api/user') ini contoh
 @Controller('user')
 export class UserController {}
